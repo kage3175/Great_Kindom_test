e8f65e8d526bcbb345dc489f29f5c37f957f0c9b
hyunsoo <hjames2000@snu.ac.kr>
2023-07-31 11:45:40 +0900

    rw

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ BLANK = 0
 BLACKSTONE = 1
 WHITESTONE = 2
 NEUTRAL = 3
+LEFT_TOP = [124, 121]
+GAP = [54.8,55]
 
 ### Pygame 관련 전역변수들
 FPS = 30
@@ -51,16 +53,33 @@ def receive(sock):
 
 def main_game():
     pygame.init()
-    screen = pygame.display.set_mode((350, 500))
-    pygame.display.set_caption("Great_Kindom_test")
+    screen=pygame.display.set_mode((800,720))
+    pygame.display.set_caption('Great Kingdom')
 
-    while 1:
-        screen.fill(WHITE)
+    fps=10 #프레임이 높을 필요가 없기 때문에 프레임은 10으로 설정하였다. 낮은 사양의 컴퓨터에서는 이를 5 정도로 낮춰서 사용해도 문제가 없다.
+    fpsClock=pygame.time.Clock()
+
+    imgBoard = pygame.image.load('./img/board.png')
+    imgBlackStone = pygame.image.load('./img/black_stone.png')
+    imgWhiteStone = pygame.image.load('./img/white_stone.png')
+    lst_imgBlackStone = []
+
+    while True:
         for event in pygame.event.get():
-            if event.type==QUIT : # Q랑 같은 역할이다. 나도 왜 이걸 따로 만들었는지 기억이 가물가물... 아... 0.1 버전 때는 버그 때문에 이거 안 먹어서 Q를 따로 만든거였어용ㅎㅎ 지금은 잘 먹음
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type==MOUSEBUTTONDOWN and event.button==1:
+                position=pygame.mouse.get_pos()
+                print(position)
+        screen.fill((255,255,255))
+        screen.blit(imgBoard, (52,49))
+        '''screen.blit(imgBlackStone, imgBlackStone_RectObj)
+        screen.blit(imgWhiteStone, imgWhiteStone_RectObj)'''
+        for i in range(9):
+            screen.blit(imgBlackStone, (round(LEFT_TOP[0]+GAP[0]*(i-1)), round(LEFT_TOP[1]+GAP[1]*(i-1))))
         pygame.display.flip()
+
 
 def start_window():
     window=Tk()
---
+++ test.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from pygame.locals import *
+
+LEFT_TOP = [124, 121]
+SPACE = [54.8,55]
+
+pygame.init()
+screen=pygame.display.set_mode((800,720))
+pygame.display.set_caption('Great Kingdom')
+
+fps=10 #프레임이 높을 필요가 없기 때문에 프레임은 10으로 설정하였다. 낮은 사양의 컴퓨터에서는 이를 5 정도로 낮춰서 사용해도 문제가 없다.
+fpsClock=pygame.time.Clock()
+
+imgBoard = pygame.image.load('./img/board.png')
+imgBlackStone = pygame.image.load('./img/black_stone.png')
+imgWhiteStone = pygame.image.load('./img/white_stone.png')
+lst_imgBlackStone = []
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type==MOUSEBUTTONDOWN and event.button==1:
+            position=pygame.mouse.get_pos()
+            print(position)
+    screen.fill((255,255,255))
+    screen.blit(imgBoard, (52,49))
+    '''screen.blit(imgBlackStone, imgBlackStone_RectObj)
+    screen.blit(imgWhiteStone, imgWhiteStone_RectObj)'''
+    for i in range(9):
+        screen.blit(imgBlackStone, (round(LEFT_TOP[0]+SPACE[0]*(i-1)), round(LEFT_TOP[1]+SPACE[1]*(i-1))))
+    pygame.display.flip()
